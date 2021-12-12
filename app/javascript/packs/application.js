409d73963d17691541d20f55ce91814ff526577f
Alvin Crespo <alvin.crespo@gmail.com>
2021-12-12 16:05:31 -0500

    setup users

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -12,3 +12,5 @@ Rails.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+
+import "controllers"
